41fa23b9fb8595e6ecb64ccf837da0865096856f
Yashwanth Sagar <thelostwatermelon@gmail.com>
2020-10-21 17:43:18 +0530

    Change title

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Container, Row, Col } from "react-bootstrap";
 import TodosForm from "../components/todos/TodosForm.jsx";
 import TodosList from "../components/todos/TodosList.jsx";
@@ -5,6 +6,9 @@ import TodosList from "../components/todos/TodosList.jsx";
 const Index = () => {
   return (
     <>
+      <Head>
+        <title>React.js, Next.js, Redux-Thunk Todos App</title>
+      </Head>
       <Container>
         <Row className="pt-5">
           <Col xs={12} md={6}>
